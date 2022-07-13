765235a4819388cdce74bd7afe630c86d89a733a
Diego Olivera <diegonose19@gmail.com>
2022-07-12 23:26:11 -0300

    Incorporando Arrays

--- JS/miarchivo.js
+++ JS/miarchivo.js
@@ -13,7 +13,7 @@
     alert(mensaje);
 
 
-}*/
+}
 
 //Simulador interactivo de compra de servicios
 
@@ -32,3 +32,69 @@ let descuento = parseInt(prompt("Ingresa tu cupón de descuento"))
 let precioFinal = calcularPrecio(servicio, cantidad, descuento);
 alert("El precio total de tu compra es de $" + precioFinal);
 alert("¡Gracias por confiar en BAJOPerflims!");
+
+*/
+
+class Servicio {
+    constructor(nombre, id, precio) {
+        this.nombre = nombre;
+        this.id = id;
+        this.precio = precio;
+    }
+}
+
+const PromoArtistica = [];
+const ProdAudiovisual = [];
+const Booking = [];
+const ProdMusical = [];
+
+PromoArtistica.push(new Servicio("Imagen", 1, 1999));
+PromoArtistica.push(new Servicio("Imagen + Redes Sociales", 2, 4000));
+PromoArtistica.push(new Servicio("Imagen + Redes Sociales + Marketing", 3, 9000));
+
+ProdAudiovisual.push(new Servicio("1 videoclip", 4, 4999));
+ProdAudiovisual.push(new Servicio("2 videoclips", 5, 8999));
+ProdAudiovisual.push(new Servicio("3 videoclips", 6, 12000));
+
+Booking.push(new Servicio("1 evento", 7, 2499));
+Booking.push(new Servicio("2 eventos", 8, 4999));
+Booking.push(new Servicio("4 eventos", 9, 10000));
+
+ProdMusical.push(new Servicio("Single", 10, 1999));
+ProdMusical.push(new Servicio("EP", 11, 4999));
+ProdMusical.push(new Servicio("Album", 12, 9999));
+
+for (const PromoArtistica of PromoArtisticas) {
+    alert("ID (" + PromoArtistica.id + ") - " + PromoArtistica.nombre);
+}
+for (const ProdAudiovisual of ProdAudiovisuales) {
+    alert("ID (" + ProdAudiovisual.id + ") - " + ProdAudiovisual.nombre);
+}
+for (const Booking of Bookings) {
+    alert("ID (" + Booking.id + ") - " + Booking.nombre);
+}
+for (const ProdMusical of ProdMusicales) {
+    alert("ID (" + ProdMusical.id + ") - " + ProdMusical.nombre);
+}
+
+let servicioSeleccionado = parseInt(prompt("Ingrese el ID del servicio que desea comprar:"));
+
+const PromoArtisticaBuscada = PromoArtistica.find(PromoArtistica => PromoArtistica.id === servicioSeleccionado);
+const ProdAudiovisualBuscada = ProdAudiovisual.find(ProdAudiovisual => ProdAudiovisual.id == servicioSeleccionado);
+const BookingBuscado = Booking.find(Booking => Booking.id == servicioSeleccionado);
+const ProdMusicalBuscada = ProdMusical.find(ProdMusical => ProdMusical.id == servicioSeleccionado);
+
+if (servicioSeleccionado <= 0) {
+    alert("Ingresa un ID valido");
+} else if (servicioSeleccionado <=3) {
+    alert("Seleccionaste " +  PromoArtisticaBuscada.nombre + "\nToca aceptar para agregarlo a tu carrito"); 
+} else if (servicioSeleccionado <=6) {
+    alert("Seleccionaste " + ProdAudiovisualBuscada.nombre + "\nToca aceptar para agregarlo a tu carrito");
+} else if (servicioSeleccionado <= 9) {
+    alert("Seleccionaste " + BookingBuscado.nombre + "\nToca aceptar para agregarlo a tu carrito");
+} else if (servicioSeleccionado <= 12) {
+    alert("Seleccionaste " + ProdMusicalBuscada.nombre + "\nToca aceptar para agregarlo a tu carrito");
+} else {
+
+    alert("Ingresa un ID valido");
+}
